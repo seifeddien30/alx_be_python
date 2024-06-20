e79caf4c2f33499c38614c493530b1a3174d4898
Sefo <40428678+seifeddien30@users.noreply.github.com>
2024-06-21 02:49:15 +0300

    Add files via upload

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+seconds = 3600
+result = hours * seconds
+print(hours, "hour(s) is", result, "seconds")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,2 @@
+interest = 1000 * 0.05 * 3
+print("The simple interest is:", interest)
