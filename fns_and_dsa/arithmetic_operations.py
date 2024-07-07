fae4d5617e981057a6adefdf1dc6f8f64ea8ed56
Sefo <40428678+seifeddien30@users.noreply.github.com>
2024-07-07 15:30:14 +0300

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+            return num1 + num2
+    if operation == 'subtract':
+            return num1 - num2
+    if operation == 'multiply':
+            return num1 * num2
+    if operation == 'divide':
+            if num2 != 0:
+                return num1 / num2
+            else: 
+                return 'You cant divide on 0'
+
